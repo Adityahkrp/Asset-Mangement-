4939edd13653f8eb63163477b7cf4e9f59467ba5
Adityahkrp <aditya.hkrp@gmail.com>
2025-11-24 11:10:44 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+// Load sidebar
+fetch("../components/sidebar.html")
+    .then(res => res.text())
+    .then(html => document.getElementById("sidebar").innerHTML = html);
+
+// Load topbar
+fetch("../components/topbar.html")
+    .then(res => res.text())
+    .then(html => document.getElementById("topbar").innerHTML = html);
 
