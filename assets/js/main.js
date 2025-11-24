6eb85fb0b1725148fa622920d30c979b02030e75
Adityahkrp <aditya.hkrp@gmail.com>
2025-11-24 11:14:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-// Load sidebar
-fetch("../components/sidebar.html")
-    .then(res => res.text())
-    .then(html => document.getElementById("sidebar").innerHTML = html);
+// Load components dynamically
+function loadComponent(id, file) {
+    fetch(file)
+        .then(res => res.text())
+        .then(html => document.getElementById(id).innerHTML = html);
+}
 
-// Load topbar
-fetch("../components/topbar.html")
-    .then(res => res.text())
-    .then(html => document.getElementById("topbar").innerHTML = html);
-
+// Inject sidebar + topbar
+loadComponent("sidebar", "../components/sidebar.html");
+loadComponent("topbar", "../components/topbar.html");
